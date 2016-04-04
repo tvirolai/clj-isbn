4ffa54c91f5594369bff7d4bd70b8584469b9897
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2016-04-04 13:01:07 +0300

    ..

--- src/clj_isbn/core.clj
+++ src/clj_isbn/core.clj
@@ -2,24 +2,26 @@
   (:gen-class))
 
 (defn normalize [isbn]
-  (apply str (filter #(Character/isDigit %) isbn)))
+  (apply str (filter #(or (Character/isDigit %) (= \X %)) isbn)))
 
 (defn charToInt [c]
-  (Character/digit c 10))
+  (if (= c \X) 10
+  (Character/digit c 10)))
 
 (defn validate [isbn]
-  (let [checkDigit (charToInt (nth isbn (- (count isbn) 1)))]
-    (loop [i 0 sum 0]
-      (let [no (charToInt (int (nth isbn i)))]
-        (if (= (count isbn) (+ 1 i))
-          ;(= (mod no 11) checkDigit)
-          (mod (+ sum (* no i)) 11)
-          (do
-            (println (str "index: " i " number: " (nth isbn i) " sum: " sum " current no: " no " " isbn " checkDigit: " checkDigit))
-            (recur (inc i) (+ sum (* no i)))))))))
+  (let [isbn (normalize isbn)]
+    (let [checkDigit (charToInt (nth isbn (- (count isbn) 1)))]
+      (loop [i 0 sum 0 pos (count isbn)]
+        (let [no (charToInt (int (nth isbn i)))]
+          (if (= pos 1)
+            ;(= (mod no 11) checkDigit)
+            (= (mod (+ sum (* checkDigit pos)) 11) 0)
+            (do
+              (println (str "index: " i " number: " (nth isbn i) " sum: " sum " current no: " no " " isbn " checkDigit: " checkDigit " pos: " pos))
+              (recur (inc i) (+ sum (* no pos)) (dec pos)))))))))
 
 
 (defn -main
   "A to-be ISBN code utility library for Clojure. A work in progress."
   [& args]
-  (println (validate (normalize "951-98548-9-1"))))
+  (println (validate "951-98548-9-1")))
