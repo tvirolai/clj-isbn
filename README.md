9ff760a4aee77fef34e0150448299c414ff9ce0f
Tuomo Virolainen <tuomo.virolainen@kapsi.fi>
2016-08-13 22:37:02 +0300

    Basic functionality

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # clj-isbn
 
-An ISBN utility library for Clojure. Currently just a project skeleton.
+An ISBN utility library for Clojure. Currently provides calculation of check digits and validity checks.
+
+TODO: Documentation, ISBN hyphenation, helpful error messages, publishing in Clojars, conversions between ISBN 10 & ISBN 13.
--- src/clj_isbn/core.clj
+++ src/clj_isbn/core.clj
@@ -1,27 +1,74 @@
 (ns clj-isbn.core
   (:gen-class))
 
-(defn normalize [isbn]
+(defn- normalize [isbn]
   (apply str (filter #(or (Character/isDigit %) (= \X %)) isbn)))
 
-(defn charToInt [c]
+(defn- charToInt [c]
   (if (= c \X) 10
     (Character/digit c 10)))
 
-(defn isbn10-checkdigit [isbn]
-  (let [isbn (normalize isbn)]
-    (loop [i 0 sum 0 pos (count isbn)]
-      (let [no (charToInt (int (nth isbn i)))]
-        (if (= pos 1)
-          (do
-            (let [digit (- 11 (mod sum 11))]
-              (cond
-                (> digit 10) 0
-                (= digit 10) "X"
-                :else digit)))
-          (recur (inc i) (+ sum (* no pos)) (dec pos)))))))
+(defn- length-correct?
+  "Takes an ISBN and checks that its length is correct.
+   In: string, out: boolean"
+  [isbn]
+  (contains? #{10 13 17} (count isbn)))
 
-(defn -main
-  "A to-be ISBN code utility library for Clojure. A work in progress."
-  [& args]
-  (println (validate "951-98548-9-1")))
+(defn- no-erroneous-chars?
+  "Takes an ISBN and checks if it contains characters that
+  don't belong to an ISBN.
+  In: string, out: boolean"
+  [isbn]
+  (length-correct? (normalize isbn)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PUBLIC FUNCTIONS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn isbn10-checkdigit 
+  "Takes an ISBN 10 code as string, returns the check digit
+   In: string, out: integer" 
+  [isbn]
+  (let [nsum
+    (->> isbn
+         (normalize)
+         (take 9)
+         (map-indexed (fn [idx itm] (* (- 10 idx) (charToInt itm))))
+         (reduce +))]
+    (mod (- 11 (mod nsum 11)) 11)))
+
+(defn isbn13-checkdigit
+  "Takes an ISBN 13 code as string, returns the check digit
+   In: string, out: integer" 
+  [isbn]
+  (let [nsum
+    (->> isbn
+         (normalize)
+         (take 12)
+         (map-indexed 
+           (fn [idx itm] 
+             (let [itm (charToInt itm)] 
+               (if (odd? idx) (* itm 3) itm))))
+         (reduce +))]
+  (- 10 (mod nsum 10))))
+
+(defn check-digit-correct?
+  "Takes and ISBN and checks its validity by the check digit
+   In: string, out: boolean"
+  [isbn]
+  (let [isbn (normalize isbn)]
+    (let [digit 
+          (if (= (count isbn) 10) 
+            (isbn10-checkdigit isbn)
+            (isbn13-checkdigit isbn))]
+      (= digit (charToInt (last isbn))))))
+
+(defn is-valid?
+  "Takes and ISBN and checks its validity by 
+  checking the check digit, length and characters.
+  In: string, out: boolean"
+  [isbn]
+  (and 
+    (check-digit-correct? isbn)
+    (length-correct? isbn)
+    (no-erroneous-chars? isbn)))
