31ae58534da4107c52ebb3219b547227fb0c8550
Tuomo Virolainen <tuomo.virolainen@siili.com>
2019-09-09 16:54:36 +0300

    Update deployment settings

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "RELEASE"]]
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :sign-releases false}]]
   :plugins [[lein-cloverage "1.1.1"]])
